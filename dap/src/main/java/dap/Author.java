7827b7b4d570e9be108920217baebada51a0844f
Younes202 <younessghyer642@gmail.com>
2022-09-15 13:01:32 +0100

    using a jstl library

---
+++ dap/src/main/java/dap/Author.java
@@ -0,0 +1,28 @@
+package dap;
+
+public class Author {
+	private String FullName;
+	private String Phone;
+	private int Age;
+	public String getFullName() {
+		return FullName;
+	}
+	public void setFullName(String fullName) {
+		FullName = fullName;
+	}
+	public String getPhone() {
+		return Phone;
+	}
+	public void setPhone(String phone) {
+		Phone = phone;
+	}
+	public int getAge() {
+		return Age;
+	}
+	public void setAge(int age) {
+		Age = age;
+	}
+
+	
+
+}
